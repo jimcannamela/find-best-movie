32d03055f860bfb878656cd868a2d08e10642e72
Connor Steiling <connor.steiling.xaa6@statefarm.com>
2023-05-03 09:17:51 -0500

    Work through as a group

--- scripts/app.js
+++ scripts/app.js
@@ -28,4 +28,45 @@ Instructions:
 
 */
 
+//Prior work to the demonstration
+// const movieList = []
 
+// for(let p = 0; p < 10; p++) {
+//    getMovie(p)
+//       .then((movie) => {
+//          return createMovieSection(movie)
+//       })
+//       .then((section) => {
+//          document.body.append(section)
+//       })
+// }
+
+// Creates a list of promises
+const movieProms = [];
+for(let m = 0; m < 10; m++){
+   movieProms.push(getMovie(nextInt(100, 1000)));
+}
+
+//Once all promises are resolved then call the callback function
+Promise.all(movieProms)
+   //Executes the callback function once all promises in the list have been resolved
+   .then(function(movies) {
+      //Sets the topRated to the first movie
+      let topRated = movies[0]
+
+      //Iterates through each movie and creates a container for each
+      for(let mov of movies){
+         mov.container = createMovieSection(mov)
+
+         //Sets the topRated variable to the highest rated movie
+         if(mov.rating > topRated.rating) {
+            topRated = mov;
+         }
+
+         //Appends eash movie to the body
+         document.body.append(mov.container)
+      }
+
+      //Once each movie has been iterated through it then appends the class name of the highest one
+      topRated.container.classList.add('highest');
+   })
